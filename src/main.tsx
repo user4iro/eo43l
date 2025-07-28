d0d82659a4957132da6915e81331aac0380f1fdd
user4iro <ventas1agrosuministros@gmail.com>
2025-07-28 09:03:47 -0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Verifica si el usuario ya pasó por el prelanding
-const acceso = new URLSearchParams(window.location.search).get("acceso");
-
-// Si viene del prelanding, entra al sitio React
-if (acceso === "true") {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  // Si NO viene del prelanding, lo manda al prelanding
-  window.location.href = "/prelanding.html";
-}
+// Cargar siempre la app principal sin prelanding
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

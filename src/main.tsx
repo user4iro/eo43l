df2578af7b595267b5170cfed9e00d845fa715e1
user4iro <ventas1agrosuministros@gmail.com>
2025-07-27 19:48:50 -0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(<App />);
+// Verifica si el usuario ya pasó por el prelanding
+const acceso = new URLSearchParams(window.location.search).get("acceso");
+
+// Si viene del prelanding, entra al sitio React
+if (acceso === "true") {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  // Si NO viene del prelanding, lo manda al prelanding
+  window.location.href = "/prelanding.html";
+}
